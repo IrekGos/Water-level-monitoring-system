55907c6102d29868413b42fd9fb3f424a96ef476
IrekGos <ireneusz.goscinski@gmail.com>
2023-11-07 19:23:06 +0100

    Add signalizing water level with diodes

---
+++ Raspberry Pi/leds.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+
+RED_LED = 17
+YELLOW_LED = 27
+GREEN_LED = 22
+
+def led_on(led: int) -> None:
+    GPIO.output(led, GPIO.HIGH)
+
+def led_off(led: int) -> None:
+    GPIO.output(led, GPIO.LOW)
+
+def led_init() -> None:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(RED_LED, GPIO.OUT)
+    GPIO.setup(YELLOW_LED, GPIO.OUT)
+    GPIO.setup(GREEN_LED, GPIO.OUT)
+
+def default_leds_setting() -> None:
+    led_on(GREEN_LED)
+    led_off(YELLOW_LED)
+    led_off(RED_LED)
--- Raspberry Pi/main.py
+++ Raspberry Pi/main.py
@@ -1,12 +1,24 @@
 from time import sleep
+import leds
 import uart
 
+WARNING_TRESHOLD = 300
+ALERT_TRESHOLD = 400
 MEASUREMENT_INTERVAL = 60
 
 if __name__=="__main__":
 
+    leds.led_init()
+
     uart.connect_with_atmega()
     
     while True:
+        leds.default_leds_setting()
         res = uart.get_measurement_result()
+        if res > ALERT_TRESHOLD:
+            leds.led_on(leds.RED_LED)
+            leds.led_off(leds.GREEN_LED)
+        elif res > WARNING_TRESHOLD:
+            leds.led_on(leds.YELLOW_LED)
+            leds.led_off(leds.GREEN_LED)
         sleep(MEASUREMENT_INTERVAL)
