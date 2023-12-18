c5abb84b334fefc4edfa20f77bffa8c84850dcb0
IrekGos <ireneusz.goscinski@gmail.com>
2023-12-18 16:43:37 +0100

    Store hashes of passwords instead of plain text

--- Raspberry Pi/database.py
+++ Raspberry Pi/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 from dotenv import dotenv_values
+from hashlib import sha3_512
 
 
 class CredentialsDB:
@@ -9,8 +10,9 @@ class CredentialsDB:
         self.__cursor = self.__connection.cursor()
 
     def authenticate(self, username: str, password: str) -> bool:
+        hashed_password = sha3_512(password.encode('utf-8')).hexdigest()
         self.__cursor.execute(
-            "SELECT EXISTS(SELECT 1 FROM CREDENTIALS WHERE Username = ? AND Password = ?)", (username, password))
+            "SELECT EXISTS(SELECT 1 FROM CREDENTIALS WHERE Username = ? AND Password = ?)", (username, hashed_password))
         user = self.__cursor.fetchall()
         assert (len(user) == 1)
         return bool(user[0][0])
